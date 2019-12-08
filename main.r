3191ade0ed67a5e5657c2b363309e21cf3bea9f9
Jose Orozco <JoseOrozco1j@gmail.com>
2019-12-08 17:10:52 +0000

    Added comments describing code

--- main.r
+++ main.r
@@ -1,27 +1,32 @@
-## Generate a random sample of 50 numbers from a normal distribution
-## with mean 10 and standard deviation of 2
-data()
-data(rivers)
-ls()
-head(rivers)
-length(rivers)
-summary(rivers)
+#Purpose: A program using the "R" language to show 
+#a pre made data set of American rivers, and place 
+#the information onto a histogram.
+
+data()# browse pre-loaded data sets
+data(rivers)# get this one: "Lengths of Major North American Rivers"
+ls() # notice that "rivers" now appears in the workspace
+head(rivers) # peek at the data set
+# 735 320 325 392 524 450
+
+length(rivers)  # how many rivers were measured?
+# 141
+summary(rivers) # summary of stats
+
+# make a stem-and-leaf plot (a histogram-like data visualization)
 stem(rivers)
 stem(log(rivers))
 
-hist(rivers, col="#333333", border= "white", breaks=25)
-hist(log(rivers), col="#333333", border="white", breaks=25)
+#begin visualization
 
-data(discoveries, col="#333333", lwd=3, xlab="Year")
-plot(discoveries, col="#333333", led=3, type="h", xlab="Year", main= "Number of discoveries per year")
+# creates a histogram:
+hist(rivers, col="#333333", border="white", breaks=25)
+#changes visualization
+hist(log(rivers), col="#333333", border="white", breaks=25) 
 
-sort(discoveries)
-stem(discoveries, scale=2)
-
-5L
-class(5L)
-length(5L)
-
-c(4L, 8L, 6L)
-length(c(4L, 5L, 6L))
-10L + 66L
+# pre loaded one
+data(discoveries)
+plot(discoveries, col="#333333", lwd=3, xlab="Year",
+     main="Number of important discoveries per year")
+plot(discoveries, col="#333333", lwd=3, type = "h", xlab="Year",
+     main="Number of important discoveries per year")
+     #practice for Reaserch - Directed Study 285 - R
